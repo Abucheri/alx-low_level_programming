1cacc34b3e762d9e1a18c2945895b9aa61a790b5
O'Brien <abucheriobrien@gmail.com>
2023-03-21 10:42:42 -0700

    list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - entry into the program
+  * Prints the sum of all multiples of 3 or 5 up to 1024
+  *
+  * Return: Always 0 for success
+  */
+int main(void)
+{
+	int a, b = 0;
+
+	while (a < 1024)
+	{
+		if ((a % 3 == 0) || (a % 5 == 0))
+		{
+			b += a;
+		}
+
+		a++;
+	}
+
+	printf("%d\n", b);
+	return (0);
+}
