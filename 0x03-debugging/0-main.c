e56e47ce33bdbc773c2f2966daeed7f49fa3e85f
O'Brien <abucheriobrien@gmail.com>
2023-03-23 06:41:30 -0700

    test for a postitive_or_negative()

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main -  entry point into the program
+ * tests if the functin prints a positive or
+ * negative integer
+ *
+ * Return: always 0 for success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	postitive_or_negative(i);
+
+	return (0);
+}
