4cb7cd67bfd1b902e8a78c0bff01aa20950cfaa7
O'Brien <abucheriobrien@gmail.com>
2023-03-30 07:38:35 -0700

    a function that encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13.
+ *
+ * @a: string to be encoded
+ *
+ * Return: encoded string
+ */
+
+char *rot13(char *a)
+{
+	int i1 = 0, i2;
+	char x[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char y[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
+
+	while (a[i1])
+	{
+		for (i2 = 0; i2 < 52; i2++)
+		{
+			if (a[i1] == x[i2])
+			{
+				a[i1] = y[i2];
+				break;
+			}
+		}
+
+		i1++;
+	}
+
+	return (a);
+}
