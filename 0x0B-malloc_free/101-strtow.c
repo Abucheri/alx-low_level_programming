50e9a368a054cbb210e73ffaf28ebb0fffebca6f
O'Brien <abucheriobrien@gmail.com>
2023-04-11 17:15:09 -0700

    a function that splits a string into words.

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -62,31 +62,28 @@ int count_words(char *str)
  * If function fails, it should return NULL
  */
 
+
 char **strtow(char *str)
 {
 	char **strings;
 	int index = 0, words, w, letters, l;
 
 	if (str == NULL || str[0] == '\0')
-	{
 		return (NULL);
-	}
+
 	words = count_words(str);
 	if (words == 0)
-	{
 		return (NULL);
-	}
+
 	strings = malloc(sizeof(char *) * (words + 1));
 	if (strings == NULL)
-	{
 		return (NULL);
-	}
+
 	for (w = 0; w < words; w++)
 	{
 		while (str[index] == ' ')
-		{
 			index++;
-		}
+
 		letters = word_len(str + index);
 
 		strings[w] = malloc(sizeof(char) * (letters + 1));
@@ -94,17 +91,15 @@ char **strtow(char *str)
 		if (strings[w] == NULL)
 		{
 			for (; w >= 0; w--)
-			{
 				free(strings[w]);
-			}
+
 			free(strings);
 			return (NULL);
 		}
 
 		for (l = 0; l < letters; l++)
-		{
 			strings[w][l] = str[index++];
-		}
+
 		strings[w][l] = '\0';
 	}
 	strings[w] = NULL;
