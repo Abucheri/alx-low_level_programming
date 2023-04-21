48edbf79a13618054c96da81c60a20dd6b1b582a
O'Brien Abucheri <abucheriobrien@gmail.com>
2023-04-21 03:04:31 +0300

    a function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,6 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 void print_char(va_list arg);
 void print_int(va_list arg);
@@ -9,80 +9,73 @@ void print_string(va_list arg);
 void print_all(const char * const format, ...);
 
 /**
- * print_char - prints a char
- *
- * @chr: list of arguments pointing to
- * the character to be printed
+ * print_char - Prints a char.
+ * @arg: A list of arguments pointing to
+ *       the character to be printed.
  */
-
-void print_char(va_list chr)
+void print_char(va_list arg)
 {
-	char l;
-	
-	l = va_arg(chr, int);
-	printf("%c", l);
+	char letter;
+
+	letter = va_arg(arg, int);
+	printf("%c", letter);
 }
 
 /**
- * print_int - prints an int
- *
- * @ints: list of arguments pointing to
- * the integer to be printed
+ * print_int - Prints an int.
+ * @arg: A list of arguments pointing to
+ *       the integer to be printed.
  */
-
-void print_int(va_list ints)
+void print_int(va_list arg)
 {
-	int n;
-	
-	n = va_arg(ints, int);
-	printf("%d", n);
+	int num;
+
+	num = va_arg(arg, int);
+	printf("%d", num);
 }
 
 /**
- * print_float - prints a float
- *
- * @fl: list of arguments pointing to
- * the float to be printed
+ * print_float - Prints a float.
+ * @arg: A list of arguments pointing to
+ *       the float to be printed.
  */
-
-void print_float(va_list fl)
+void print_float(va_list arg)
 {
-	float a;
-	
-	a = va_arg(fl, double);
-	printf("%f", a);
+	float num;
+
+	num = va_arg(arg, double);
+	printf("%f", num);
 }
 
 /**
- * print_string - prints a string
- *
- * @strs: list of arguments pointing to
- * the string to be printed
+ * print_string - Prints a string.
+ * @arg: A list of arguments pointing to
+ *       the string to be printed.
  */
-
-void print_string(va_list strs)
+void print_string(va_list arg)
 {
-	char *s;
-	
-	s = va_arg(strs, char *);
-	
-	if (s == NULL)
+	char *str;
+
+	str = va_arg(arg, char *);
+
+	if (str == NULL)
 	{
 		printf("(nil)");
 		return;
 	}
-	printf("%s", s);
+
+	printf("%s", str);
 }
 
 /**
- * print_all - prints anything
+ * print_all - prints anything, followed by a new line.
+ * @format: string of characters representing the argument types.
+ * @...: variable number of arguments to be printed.
  *
- * @format: string of characters representing the argument types
- * @...: variable number of arguments to be printed
- *
- * Return: any argument
+ * Description: any argument not of type char, int, float,
+ * or char * is ignored.
+ * If a string argument is NULL, (nil) is printed instead.
  */
-
 void print_all(const char * const format, ...)
 {
 	va_list args;
@@ -94,24 +87,27 @@ void print_all(const char * const format, ...)
 		{"f", print_float},
 		{"s", print_string}
 	};
-	
+
 	va_start(args, format);
-	
+
 	while (format && (*(format + i)))
 	{
 		j = 0;
+
 		while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
-		{
 			j++;
-		}
+
 		if (j < 4)
 		{
 			printf("%s", separator);
 			funcs[j].print(args);
 			separator = ", ";
 		}
+
 		i++;
 	}
+
 	printf("\n");
+
 	va_end(args);
 }
