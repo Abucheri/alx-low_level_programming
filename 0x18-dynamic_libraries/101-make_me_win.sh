c5145d36f5052de4f159634ccd20bc6a14a3796e
O'Brien <abucheriobrien@gmail.com>
2023-09-19 01:12:24 +0000

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -shared -o my-jackpot.so -fPIC my-jackpot.c
-export LD_PRELOAD=/$PWD/my-jackpot.so
+gcc -shared -o my-jackpot.so -fPIC my-jackpot.c -ldl
+LD_PRELOAD=/$PWD/my-jackpot.so ./gm 9 8 10 24 75 9
