7d19da7bac029d2171385a9ef54c191103357ac5
O'Brien <abucheriobrien@gmail.com>
2023-09-19 08:21:57 +0000

    jackpot update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -shared -o my-jackpot.so -fPIC my-jackpot.c -ldl
-export LD_PRELOAD=/$PWD/my-jackpot.so
+gcc -shared -o myjackpot.so -fPIC myjackpot.c -ldl
+export LD_PRELOAD=/$PWD/myjackpot.so
