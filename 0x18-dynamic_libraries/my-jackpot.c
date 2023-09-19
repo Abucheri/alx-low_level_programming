bb8a9f0822f3b0e6028c06c0532ec00f351ca160
O'Brien <abucheriobrien@gmail.com>
2023-09-19 00:06:10 +0000

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/my-jackpot.c
+++ 0x18-dynamic_libraries/my-jackpot.c
@@ -13,7 +13,7 @@
 
 int print_jackpot(const char *fmt, ...)
 {
-	write(1, "9 8 10 24 75 - 9\n", 17);
-	write(1, "Congratulations, you win the jackpot!\n", 38);
+	write(1, "9 8 10 24 75 - 9\n",17);
+	write(1, "Congratulations, you win the Jackpot!\n", 38);
 	exit(EXIT_SUCCESS);
 }
